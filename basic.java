b68e1063dec0423500dd00739aba41c14bde1e48
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-12 12:48:48 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -1,3 +1,18 @@
-// activity is implemented as a subclass of Activity class:
+// 1.activity is implemented as a subclass of Activity class:
 public class MyActivity extends Activity {
+}
+//2. service is implemented as a subclass of Service
+public class MyService extends Service {
+}
+
+//3.A broadcast receiver is implemented as a subclass of BroadcastReceiver class 
+//and each message is broadcaster as an Intent object.
+public class MyReceiver  extends  BroadcastReceiver {
+   public void onReceive(context,intent){}
+}
+
+//4. content provider is implemented as a subclass of ContentProvider class
+//and must implement a standard set of APIs that enable other applications to perform transactions.
+public class MyContentProvider extends  ContentProvider {
+   public void onCreate(){}
 }
